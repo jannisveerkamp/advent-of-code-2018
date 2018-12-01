aee8ef2dba02ffe62d479999724fcdef577828b1
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-01 20:28:36 +0100

    Code style and speed up tests alot

--- 01/calibration.py
+++ 01/calibration.py
@@ -1,20 +1,21 @@
-def frequency(input):
-    sum = 0
-    for number in input:
-        sum += int(number)
-    return sum
+def frequency(changes):
+    return sum(map_list_to_int(changes))
 
 
-def frequency_twice(input):
-    seen_frequencies = [0]
+def frequency_twice(changes):
+    changes = map_list_to_int(changes)
+    seen_frequencies = {0}
     index = 0
     current_frequency = 0
 
     while True:
-        current_frequency += int(input[index])
-        index = (index + 1) % len(input)
+        current_frequency += changes[index]
+        index = (index + 1) % len(changes)
         if current_frequency in seen_frequencies:
             return current_frequency
         else:
-            seen_frequencies.append(current_frequency)
-    
+            seen_frequencies.add(current_frequency)
+
+
+def map_list_to_int(changes):
+    return list(map(int, changes))
