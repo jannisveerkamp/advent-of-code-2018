31112ae4157d5303ee27e32d56218d81062aa774
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-03 10:42:20 +0100

    Code style and speed improvements

--- 03/square.py
+++ 03/square.py
@@ -1,47 +1,46 @@
 import re
 
-pattern = re.compile("#(?P<number>\d*) @ (?P<left>\d*),(?P<top>\d*): (?P<width>\d*)x(?P<height>\d*)")
+
+class Claim:
+    pattern = re.compile("#(?P<number>\d*) @ (?P<left>\d*),(?P<top>\d*): (?P<width>\d*)x(?P<height>\d*)")
+
+    def __init__(self, claim):
+        match = self.pattern.match(claim)
+        self.number = int(match.group("number"))
+        self.left = int(match.group("left"))
+        self.top = int(match.group("top"))
+        self.width = int(match.group("width"))
+        self.height = int(match.group("height"))
 
 
 def overlap(claims):
-    matrix = fill_matrix(claims)
+    claim_list = [Claim(claim) for claim in claims]
+    matrix = __fill_matrix(claim_list)
     return sum(sum(y > 1 for y in x) for x in matrix)
 
 
 def no_overlap(claims):
-    matrix = fill_matrix(claims)
+    claim_list = [Claim(claim) for claim in claims]
+    matrix = __fill_matrix(claim_list)
 
-    for claim in claims:
-        match = pattern.match(claim)
-        number = int(match.group("number"))
-        left = int(match.group("left"))
-        top = int(match.group("top"))
-        width = int(match.group("width"))
-        height = int(match.group("height"))
-
-        has_overlap = False
-        for x in range(left, left + width):
-            for y in range(top, top + height):
-                if matrix[x][y] != 1:
-                    has_overlap = True
-
-        if not has_overlap:
-            return number
-
+    for claim in claim_list:
+        if not find_overlap(claim, matrix):
+            return claim.number
     return -1
 
 
-def fill_matrix(claims):
+def find_overlap(claim, matrix):
+    for x in range(claim.left, claim.left + claim.width):
+        for y in range(claim.top, claim.top + claim.height):
+            if matrix[x][y] != 1:
+                return True
+    return False
+
+
+def __fill_matrix(claims):
     matrix = [[0 for _ in range(1000)] for _ in range(1000)]
-
     for claim in claims:
-        match = pattern.match(claim)
-        left = int(match.group("left"))
-        top = int(match.group("top"))
-        width = int(match.group("width"))
-        height = int(match.group("height"))
-
-        for x in range(left, left + width):
-            for y in range(top, top + height):
+        for x in range(claim.left, claim.left + claim.width):
+            for y in range(claim.top, claim.top + claim.height):
                 matrix[x][y] += 1
     return matrix
