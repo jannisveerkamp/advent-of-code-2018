db2285d25cf329a3a30faf1844b7d7bbfb2c8bda
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-03 09:58:36 +0100

    Add solution for task 1

--- 03/square.py
+++ 03/square.py
@@ -1,2 +1,20 @@
-def overlap(box_ids):
-    return -1
+import re
+
+pattern = re.compile(".* @ (?P<left>\d*),(?P<top>\d*): (?P<width>\d*)x(?P<height>\d*)")
+
+
+def overlap(claims):
+    matrix = [[0 for _ in range(1000)] for _ in range(1000)]
+
+    for claim in claims:
+        match = pattern.match(claim)
+        left = int(match.group("left"))
+        top = int(match.group("top"))
+        width = int(match.group("width"))
+        height = int(match.group("height"))
+
+        for x in range(left, left + width):
+            for y in range(top, top + height):
+                matrix[x][y] += 1
+
+    return sum(sum(y > 1 for y in x) for x in matrix)
--- 03/square_test.py
+++ 03/square_test.py
@@ -12,4 +12,4 @@ class TestSquare(unittest.TestCase):
 
     def test_checksum_input(self):
         test_input = read_file_lines(__file__, "input.txt")
-        self.assertEqual(overlap(test_input), -1)
+        self.assertEqual(overlap(test_input), 111326)
