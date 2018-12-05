5efdb2b7659f7e78b026899793e1cf1590b8ce10
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-05 07:58:52 +0100

    Add slow solution for task 1
    Add test input

--- 05/polymer.py
+++ 05/polymer.py
@@ -1,2 +1,15 @@
 def reduce_polymer(polymer):
-    return -1
+    should_remove = True
+    while should_remove:
+        should_remove = False
+        for i in range(len(polymer) - 1):
+            if should_remove_characters(polymer[i], polymer[i + 1]):
+                polymer = polymer[:i] + polymer[i + 2:]
+                should_remove = True
+                break
+
+    return len(polymer)
+
+
+def should_remove_characters(a, b):
+    return a.lower() == b.lower() and a.isupper() != b.isupper()
--- 05/polymer_test.py
+++ 05/polymer_test.py
@@ -1,31 +1,42 @@
 import unittest
 
-from common.file import read_file_lines
-from .polymer import reduce_polymer
+from common.file import read_file
+from .polymer import reduce_polymer, should_remove_characters
 
 
 class TestPolymerReduce(unittest.TestCase):
 
+    def test_should_remove_characters(self):
+        self.assertEqual(should_remove_characters('a', 'A'), True)
+        self.assertEqual(should_remove_characters('b', 'B'), True)
+        self.assertEqual(should_remove_characters('a', 'a'), False)
+        self.assertEqual(should_remove_characters('A', 'A'), False)
+        self.assertEqual(should_remove_characters('A', 'a'), True)
+        self.assertEqual(should_remove_characters('a', 'b'), False)
+        self.assertEqual(should_remove_characters('a', 'B'), False)
+        self.assertEqual(should_remove_characters('A', 'b'), False)
+        self.assertEqual(should_remove_characters('A', 'B'), False)
+
     def test_simple_reduce_polymer_1(self):
-        test_input = ["aA"]
+        test_input = "aA"
         self.assertEqual(reduce_polymer(test_input), 0)
 
     def test_simple_reduce_polymer_2(self):
-        test_input = ["abBA"]
+        test_input = "abBA"
         self.assertEqual(reduce_polymer(test_input), 0)
 
     def test_simple_reduce_polymer_3(self):
-        test_input = ["abAB"]
+        test_input = "abAB"
         self.assertEqual(reduce_polymer(test_input), 4)
 
     def test_simple_reduce_polymer_4(self):
-        test_input = ["aabAAB"]
+        test_input = "aabAAB"
         self.assertEqual(reduce_polymer(test_input), 6)
 
     def test_simple_reduce_polymer_4(self):
-        test_input = ["dabAcCaCBAcCcaDA"]
+        test_input = "dabAcCaCBAcCcaDA"
         self.assertEqual(reduce_polymer(test_input), 10)
 
     def test_reduce_polymer_input(self):
-        test_input = read_file_lines(__file__, "input.txt")
-        self.assertEqual(reduce_polymer(test_input), 0)
+        test_input = read_file(__file__, "input.txt")
+        self.assertEqual(reduce_polymer(test_input), 11754)
