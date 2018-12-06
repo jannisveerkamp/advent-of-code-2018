802126a1ab343521728eab6ea47cfcee7eab6dbe
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-06 11:02:20 +0100

    Add tests for Manhattan distance

--- 06/coordinates_test.py
+++ 06/coordinates_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from common.file import read_file_lines
-from .coordinates import size_largest_area, get_array_size, parse_coordinates, size_safe_are
+from .coordinates import size_largest_area, get_array_size, parse_coordinates, size_safe_are, manhattan_distance
 
 
 class TestLargestArea(unittest.TestCase):
@@ -25,6 +25,17 @@ class TestLargestArea(unittest.TestCase):
 
 
 class TestSafeArea(unittest.TestCase):
+
+    def test_manhattan_distance(self):
+        self.assertEqual(manhattan_distance(0, 0, 0, 0), 0)
+        self.assertEqual(manhattan_distance(5, 5, 5, 5), 0)
+        self.assertEqual(manhattan_distance(1, 1, 0, 0), 2)
+        self.assertEqual(manhattan_distance(0, 0, 1, 1), 2)
+        self.assertEqual(manhattan_distance(5, 5, 0, 0), 10)
+        self.assertEqual(manhattan_distance(0, 0, 5, 5), 10)
+        self.assertEqual(manhattan_distance(1, 2, 3, 4), 4)
+        self.assertEqual(manhattan_distance(5, 13, 21, 3), 26)
+        self.assertEqual(manhattan_distance(-1, -5, 3, -7), 6)
 
     def test_simple_safe_area(self):
         test_input = ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
