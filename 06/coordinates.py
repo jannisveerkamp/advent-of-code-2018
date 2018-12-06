2f5caea20d13205ff8f717af33ae200ce20e4e3b
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-06 08:00:17 +0100

    Add helper methods

--- 06/coordinates.py
+++ 06/coordinates.py
@@ -1,2 +1,18 @@
-def size_largest_area(polymer):
+def size_largest_area(coordinates):
+    coordinates = parse_coordinates(coordinates)
+    xMax, yMax = get_array_size(coordinates)
     return -1
+
+
+def get_array_size(coordinates):
+    max_x = max(coordinates, key=lambda c: c[0])[0]
+    max_y = max(coordinates, key=lambda c: c[1])[1]
+    return max_x, max_y
+
+
+def parse_coordinates(coordinates):
+    parsed_coordinates = []
+    for coordinate in coordinates:
+        x, y = coordinate.split(", ")
+        parsed_coordinates.append([int(x), int(y)])
+    return parsed_coordinates
--- 06/coordinates_test.py
+++ 06/coordinates_test.py
@@ -1,18 +1,21 @@
 import unittest
 
 from common.file import read_file
-from .coordinates import size_largest_area
+from .coordinates import size_largest_area, get_array_size, parse_coordinates
 
 
 class TestLargestArea(unittest.TestCase):
 
+    def test_parse_coordinates(self):
+        test_input = ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
+        self.assertEqual(parse_coordinates(test_input), [[1, 1], [1, 6], [8, 3], [3, 4], [5, 5], [8, 9]])
+
+    def test_get_array_size(self):
+        test_input = [[1, 1], [1, 6], [8, 3], [3, 4], [5, 5], [8, 9]]
+        self.assertEqual(get_array_size(test_input), (8, 9))
+
     def test_simple_size_largest_area(self):
-        test_input = ["1, 1",
-                      "1, 6",
-                      "8, 3",
-                      "3, 4",
-                      "5, 5",
-                      "8, 9"]
+        test_input = ["1, 1", "1, 6", "8, 3", "3, 4", "5, 5", "8, 9"]
         self.assertEqual(size_largest_area(test_input), 17)
 
     def test_size_largest_area_input(self):
