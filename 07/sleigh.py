9a8b2572a621d337117200178c522cb2f8414c20
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-07 16:52:11 +0100

    Add solution for task 1

--- 07/sleigh.py
+++ 07/sleigh.py
@@ -1,2 +1,54 @@
+import re
+
+
 def sort_instructions(instructions):
-    return -1
+    instructions = parse_instructions(instructions)
+    letters = sorted_letters(instructions)
+    reverse_instructions = reverse(instructions)
+
+    result = []
+    while len(result) < len(letters):
+        for letter in letters:
+            # We already sorted those
+            if letter in result:
+                continue
+
+            # Append root node
+            if letter not in reverse_instructions:
+                result.append(letter)
+                break
+
+            # Append if all "sources" reached
+            if all(dst in result for dst in reverse_instructions[letter]):
+                result.append(letter)
+    return "".join(result)
+
+
+pattern = re.compile("Step (?P<step>\w) must be finished before step (?P<before>\w) can begin.")
+
+
+def parse_instructions(instructions):
+    parsed_instructions = []
+    for instruction in instructions:
+        match = pattern.match(instruction)
+        parsed_instructions.append((match.group("step"), match.group("before")))
+    return parsed_instructions
+
+
+def sorted_letters(instructions):
+    letters = set()
+    for instruction in instructions:
+        letters.add(instruction[0])
+        letters.add(instruction[1])
+    letters = list(letters)
+    letters.sort()
+    return letters
+
+
+def reverse(instructions):
+    reverse_instructions = dict()
+    for instruction in instructions:
+        if instruction[1] not in reverse_instructions:
+            reverse_instructions[instruction[1]] = set()
+        reverse_instructions[instruction[1]].add(instruction[0])
+    return reverse_instructions
--- 07/sleigh_test.py
+++ 07/sleigh_test.py
@@ -1,22 +1,32 @@
 import unittest
 
 from common.file import read_file_lines
-from .sleigh import sort_instructions
+from .sleigh import sort_instructions, parse_instructions
 
 
 class TestLargestArea(unittest.TestCase):
 
+    def test_parsing(self):
+        test_input = ["Step C must be finished before step A can begin.",
+                      "Step C must be finished before step F can begin.",
+                      "Step A must be finished before step B can begin.",
+                      "Step A must be finished before step D can begin.",
+                      "Step B must be finished before step E can begin.",
+                      "Step D must be finished before step E can begin.",
+                      "Step F must be finished before step E can begin."]
+        self.assertEqual(parse_instructions(test_input),
+                         [('C', 'A'), ('C', 'F'), ('A', 'B'), ('A', 'D'), ('B', 'E'), ('D', 'E'), ('F', 'E')])
+
     def test_simple_dummy(self):
-        self.begin__ = ["Step C must be finished before step A can begin.",
-                        "Step C must be finished before step F can begin.",
-                        "Step A must be finished before step B can begin.",
-                        "Step A must be finished before step D can begin.",
-                        "Step B must be finished before step E can begin.",
-                        "Step D must be finished before step E can begin.",
-                        "Step F must be finished before step E can begin."]
-        test_input = self.begin__
-        self.assertEqual(sort_instructions(test_input), 0)
+        test_input = ["Step C must be finished before step A can begin.",
+                      "Step C must be finished before step F can begin.",
+                      "Step A must be finished before step B can begin.",
+                      "Step A must be finished before step D can begin.",
+                      "Step B must be finished before step E can begin.",
+                      "Step D must be finished before step E can begin.",
+                      "Step F must be finished before step E can begin."]
+        self.assertEqual("CABDFE", sort_instructions(test_input))
 
     def test_size_largest_area_input(self):
         test_input = read_file_lines(__file__, "input.txt")
-        self.assertEqual(sort_instructions(test_input), 0)
+        self.assertEqual(sort_instructions(test_input), "GKCNPTVHIRYDUJMSXFBQLOAEWZ")
