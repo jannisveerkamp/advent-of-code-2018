b720ee9ca6a52b04d410a1c17831b895c1d2f9ed
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-08 20:30:01 +0100

    Add solution for task 1

--- 08/day_08.py
+++ 08/day_08.py
@@ -1,2 +1,24 @@
+class Node:
+    def __init__(self, instructions):
+        self.header_child_nodes = instructions[0]
+        self.header_metadata_entries = instructions[1]
+        self.child_nodes = []
+        i = 0
+        current_position = 2
+        while i < self.header_child_nodes:
+            new_node = Node(instructions[current_position:])
+            self.child_nodes.append(new_node)
+            current_position += new_node.size
+            i += 1
+        child_sizes = sum(node.size for node in self.child_nodes)
+        self.metadata_entries = instructions[child_sizes + 2:child_sizes + 2 + self.header_metadata_entries]
+        self.size = 2 + len(self.metadata_entries) + child_sizes
+
+    def sum_metadata_entries(self):
+        return sum(i for i in self.metadata_entries) + sum(node.sum_metadata_entries() for node in self.child_nodes)
+
+
 def day_08_task_1(instructions):
-    return -1
+    instructions = list(map(int, instructions.split(" ")))
+    tree = Node(instructions)
+    return tree.sum_metadata_entries()
--- 08/day_08_test.py
+++ 08/day_08_test.py
@@ -1,15 +1,15 @@
 import unittest
 
-from common.file import read_file_lines
+from common.file import read_file
 from .day_08 import day_08_task_1
 
 
 class TestDay08Task1(unittest.TestCase):
 
     def test_day_08_task_1_simple(self):
-        test_input = ["2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"]
+        test_input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
         self.assertEqual(138, day_08_task_1(test_input))
 
     def test_day_08_task_1__input(self):
-        test_input = read_file_lines(__file__, "input.txt")
-        self.assertEqual(0, day_08_task_1(test_input))
+        test_input = read_file(__file__, "input.txt")
+        self.assertEqual(49426, day_08_task_1(test_input))
