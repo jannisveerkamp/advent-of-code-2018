9986a1bd4aff65590dfffb4cb6db745e353b7bda
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-09 09:08:51 +0100

    Correct test cases

--- 09/day_09_test.py
+++ 09/day_09_test.py
@@ -6,28 +6,28 @@ from .day_09 import day_09_task_1
 
 class TestDay09Task1(unittest.TestCase):
 
-    def test_day_09x_task_1_simple(self):
-        test_input = ["9 players; last marble is worth 25 points"]
+    def test_day_09x_task_1_simple_1(self):
+        test_input = "9 players; last marble is worth 25 points"
         self.assertEqual(32, day_09_task_1(test_input))
 
-    def test_day_09x_task_1_simple(self):
-        test_input = ["10 players; last marble is worth 1618 points"]
+    def test_day_09x_task_1_simple_2(self):
+        test_input = "10 players; last marble is worth 1618 points"
         self.assertEqual(8317, day_09_task_1(test_input))
 
-    def test_day_09x_task_1_simple(self):
-        test_input = ["13 players; last marble is worth 7999 points"]
+    def test_day_09x_task_1_simple_3(self):
+        test_input = "13 players; last marble is worth 7999 points"
         self.assertEqual(146373, day_09_task_1(test_input))
 
-    def test_day_09x_task_1_simple(self):
-        test_input = ["17 players; last marble is worth 1104 points"]
+    def test_day_09x_task_1_simple_4(self):
+        test_input = "17 players; last marble is worth 1104 points"
         self.assertEqual(2764, day_09_task_1(test_input))
 
-    def test_day_09x_task_1_simple(self):
-        test_input = ["21 players; last marble is worth 6111 points"]
+    def test_day_09x_task_1_simple_5(self):
+        test_input = "21 players; last marble is worth 6111 points"
         self.assertEqual(54718, day_09_task_1(test_input))
 
-    def test_day_09x_task_1_simple(self):
-        test_input = ["30 players; last marble is worth 5807 points"]
+    def test_day_09x_task_1_simple_6(self):
+        test_input = "30 players; last marble is worth 5807 points"
         self.assertEqual(37305, day_09_task_1(test_input))
 
     def test_day_09_task_1__input(self):
