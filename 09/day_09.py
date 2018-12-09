88918c7446efab530eae90a3ac59a310c216f39e
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-09 09:49:39 +0100

    Speed up things with a deque and solve part 2

--- 09/day_09.py
+++ 09/day_09.py
@@ -1,5 +1,5 @@
 import re
-from collections import defaultdict
+from collections import defaultdict, deque
 
 pattern = re.compile("(?P<players>\d*) players; last marble is worth (?P<last_marble>\d*) points")
 
@@ -9,23 +9,22 @@ def parse_game_rule(game_rule):
     return int(match.group("players")), int(match.group("last_marble"))
 
 
-def day_09_task_1(game_rule):
+def day_09(game_rule, multiplier=1):
     players, last_marble = parse_game_rule(game_rule)
+    last_marble *= multiplier
 
     player_scores = defaultdict(int)
     current_player = 0
-    position = 0
-    circle = [0]
+    circle = deque([0])
 
     for current_marble in range(1, last_marble + 1):
         if current_marble % 23 == 0:
-            position = position - 7
-            if position < 0:
-                position = len(circle) + position
-            player_scores[current_player] += current_marble + circle.pop(position)
+            circle.rotate(-7)
+            player_scores[current_player] += current_marble + circle.popleft()
+            circle.rotate(1)
         else:
-            position = ((position + 1) % len(circle)) + 1
-            circle.insert(position, current_marble)
+            circle.rotate(1)
+            circle.appendleft(current_marble)
         current_player = (current_player + 1) % players
 
     return max(player_scores.values())
--- 09/day_09_test.py
+++ 09/day_09_test.py
@@ -1,35 +1,39 @@
 import unittest
 
 from common.file import read_file
-from .day_09 import day_09_task_1
+from .day_09 import day_09
 
 
-class TestDay09Task1(unittest.TestCase):
+class TestDay09(unittest.TestCase):
 
     def test_day_09x_task_1_simple_1(self):
         test_input = "9 players; last marble is worth 25 points"
-        self.assertEqual(32, day_09_task_1(test_input))
+        self.assertEqual(32, day_09(test_input))
 
     def test_day_09x_task_1_simple_2(self):
         test_input = "10 players; last marble is worth 1618 points"
-        self.assertEqual(8317, day_09_task_1(test_input))
+        self.assertEqual(8317, day_09(test_input))
 
     def test_day_09x_task_1_simple_3(self):
         test_input = "13 players; last marble is worth 7999 points"
-        self.assertEqual(146373, day_09_task_1(test_input))
+        self.assertEqual(146373, day_09(test_input))
 
     def test_day_09x_task_1_simple_4(self):
         test_input = "17 players; last marble is worth 1104 points"
-        self.assertEqual(2764, day_09_task_1(test_input))
+        self.assertEqual(2764, day_09(test_input))
 
     def test_day_09x_task_1_simple_5(self):
         test_input = "21 players; last marble is worth 6111 points"
-        self.assertEqual(54718, day_09_task_1(test_input))
+        self.assertEqual(54718, day_09(test_input))
 
     def test_day_09x_task_1_simple_6(self):
         test_input = "30 players; last marble is worth 5807 points"
-        self.assertEqual(37305, day_09_task_1(test_input))
+        self.assertEqual(37305, day_09(test_input))
 
     def test_day_09_task_1__input(self):
         test_input = read_file(__file__, "input.txt")
-        self.assertEqual(367634, day_09_task_1(test_input))
+        self.assertEqual(367634, day_09(test_input))
+
+    def test_day_09_task_2__input(self):
+        test_input = read_file(__file__, "input.txt")
+        self.assertEqual(3020072891, day_09(test_input, 100))
