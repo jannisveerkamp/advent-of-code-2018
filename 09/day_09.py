5c213430cd8ca31538ea5ca00e0c2275bfd4a0b9
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-09 08:58:27 +0100

    Parse game rules

--- 09/day_09.py
+++ 09/day_09.py
@@ -1,2 +1,13 @@
+import re
+
+pattern = re.compile("(?P<players>\d*) players; last marble is worth (?P<last_marble>\d*) points")
+
+
+def parse_game_rule(game_rule):
+    match = pattern.match(game_rule)
+    return int(match.group("players")), int(match.group("last_marble"))
+
+
 def day_09_task_1(game_rule):
+    players, last_marble = parse_game_rule(game_rule)
     return -1
