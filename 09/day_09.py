95f519b4cc2f82d9286353d052ef45737139bb3b
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-09 08:54:26 +0100

    Add failing test for day 9 task 1

---
+++ 09/day_09.py
@@ -0,0 +1,2 @@
+def day_09_task_1(game_rule):
+    return -1
---
+++ 09/day_09_test.py
@@ -0,0 +1,35 @@
+import unittest
+
+from common.file import read_file
+from .day_09 import day_09_task_1
+
+
+class TestDay09Task1(unittest.TestCase):
+
+    def test_day_09x_task_1_simple(self):
+        test_input = ["9 players; last marble is worth 25 points"]
+        self.assertEqual(32, day_09_task_1(test_input))
+
+    def test_day_09x_task_1_simple(self):
+        test_input = ["10 players; last marble is worth 1618 points"]
+        self.assertEqual(8317, day_09_task_1(test_input))
+
+    def test_day_09x_task_1_simple(self):
+        test_input = ["13 players; last marble is worth 7999 points"]
+        self.assertEqual(146373, day_09_task_1(test_input))
+
+    def test_day_09x_task_1_simple(self):
+        test_input = ["17 players; last marble is worth 1104 points"]
+        self.assertEqual(2764, day_09_task_1(test_input))
+
+    def test_day_09x_task_1_simple(self):
+        test_input = ["21 players; last marble is worth 6111 points"]
+        self.assertEqual(54718, day_09_task_1(test_input))
+
+    def test_day_09x_task_1_simple(self):
+        test_input = ["30 players; last marble is worth 5807 points"]
+        self.assertEqual(37305, day_09_task_1(test_input))
+
+    def test_day_09_task_1__input(self):
+        test_input = read_file(__file__, "input.txt")
+        self.assertEqual(0, day_09_task_1(test_input))
