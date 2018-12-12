ee8bfb5bd0cfd246792df41d685b08c019fc4e52
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-12 10:54:10 +0100

    Add solutions for part 1 and 2

--- 12/day_12.py
+++ 12/day_12.py
@@ -1,2 +1,43 @@
-def day_12_task_1(instructions, generations):
-    return -1
+MAX_GENERATIONS = 100  # Try & error for this value
+EXTENSION_SIZE = MAX_GENERATIONS + 1
+
+
+def day_12(instructions, generations):
+    current_state, instructions = parse_instructions(instructions)
+    current_state = expand(current_state)
+
+    total_range = min(100, generations)
+
+    for generation in range(total_range):
+        new_state = current_state.copy()
+        for pot_index in range(2, len(current_state) - 2):
+            current_area = "".join(current_state[pot_index - 2: pot_index + 3])
+            if current_area in instructions:
+                new_state[pot_index] = instructions[current_area]
+            else:
+                new_state[pot_index] = "."
+        current_state = new_state.copy()
+
+    return pot_sum(current_state) + (generations - total_range) * 62  # 62 only relevant for part 2, depends on input
+
+
+def expand(current_state):
+    pre = ["."] * EXTENSION_SIZE
+    post = ["."] * EXTENSION_SIZE
+    pre.extend(current_state)
+    pre.extend(post)
+    return pre
+
+
+def pot_sum(state):
+    return sum(i - EXTENSION_SIZE if state[i] == "#" else 0 for i in range(len(state)))
+
+
+def parse_instructions(instructions):
+    initial_state = instructions[0].replace("initial state: ", "")
+
+    parsed_instructions = dict()
+    for i in range(2, len(instructions)):
+        parsed_instructions[instructions[i][0:5]] = instructions[i][-1]
+
+    return initial_state, parsed_instructions
--- 12/day_12_test.py
+++ 12/day_12_test.py
@@ -1,14 +1,23 @@
 import unittest
 
 from common.file import read_file_lines
-from .day_12 import day_12_task_1
+from .day_12 import day_12, parse_instructions
 
 
 class TestDay12Task1(unittest.TestCase):
 
+    def test_day_12_parse_instructions(self):
+        test_input = ["initial state: #..#.#..##......###...###",
+                      "",
+                      "...## => #",
+                      "..#.. => ."]
+        state, instructions = parse_instructions(test_input)
+        self.assertEqual("#..#.#..##......###...###", state)
+        self.assertEqual({"...##": "#", "..#..": "."}, instructions)
+
     def test_day_12_task_1_simple(self):
         test_input = ["initial state: #..#.#..##......###...###",
-                      ""
+                      "",
                       "...## => #",
                       "..#.. => #",
                       ".#... => #",
@@ -23,8 +32,12 @@ class TestDay12Task1(unittest.TestCase):
                       "###.. => #",
                       "###.# => #",
                       "####. => #"]
-        self.assertEqual(325, day_12_task_1(test_input, 20))
+        self.assertEqual(325, day_12(test_input, 20))
 
     def test_day_12_task_1_input(self):
         test_input = read_file_lines(__file__, "input.txt")
-        self.assertEqual(0, day_12_task_1(test_input, 20))
+        self.assertEqual(2571, day_12(test_input, 20))
+
+    def test_day_12_task_2_input(self):
+        test_input = read_file_lines(__file__, "input.txt")
+        self.assertEqual(3100000000655, day_12(test_input, 50000000000))
