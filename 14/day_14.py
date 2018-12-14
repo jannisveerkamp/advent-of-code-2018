aedcce4cb612a81593f33fce84f9b800bb47a827
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-14 08:04:22 +0100

    Add failing test for day 14 task 1

---
+++ 14/day_14.py
@@ -0,0 +1,2 @@
+def day_14_task_1(instructions):
+    return ""
---
+++ 14/day_14_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from .day_14 import day_14_task_1
+
+
+class TestDay14Task1(unittest.TestCase):
+
+    def test_day_14_task_1_simple_1(self):
+        self.assertEqual("0124515891", day_14_task_1(5))
+
+    def test_day_14_task_1_simple_2(self):
+        self.assertEqual("5158916779", day_14_task_1(9))
+
+    def test_day_14_task_1_simple_3(self):
+        self.assertEqual("9251071085", day_14_task_1(18))
+
+    def test_day_14_task_1_simple_4(self):
+        self.assertEqual("5941429882", day_14_task_1(2018))
+
+    @unittest.skip("speedy!")
+    def test_day_14_task_1_input(self):
+        self.assertEqual("fail", day_14_task_1(825401))
