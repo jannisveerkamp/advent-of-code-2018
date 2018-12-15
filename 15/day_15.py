cc9b5bce9f7eddea1e213acc84bf91fcfc298a42
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-15 10:12:39 +0100

    Split and print grid

--- 15/day_15.py
+++ 15/day_15.py
@@ -1,2 +1,14 @@
 def day_15_task_1(grid):
+    grid = split_grid(grid)
+    print_grid(grid)
     return -1
+
+
+def split_grid(grid):
+    return list(map(lambda row: list(row), grid))
+
+
+def print_grid(grid):
+    for row in grid:
+        print("".join(row))
+    print()
