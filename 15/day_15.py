04d46a1ed69fd80d7b1708615389e38de2eae580
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-15 10:19:36 +0100

    Add unit class and parse initial units

--- 15/day_15.py
+++ 15/day_15.py
@@ -1,7 +1,41 @@
+GOBLIN = "G"
+ELF = "E"
+FLOOR = "."
+WALL = "#"
+
+
+class Unit:
+    def __init__(self, row, column, is_elf):
+        self.row = row
+        self.column = column
+        self.is_elf = is_elf
+        self.hp = 200
+        self.attack_power = 3
+
+    def __lt__(self, other):
+        if self.row == other.row:
+            return self.column < other.column
+        return self.row < other.row
+
+
 def day_15_task_1(grid):
     grid = split_grid(grid)
+    units = find_units(grid)
+
     print_grid(grid)
+
     return -1
+
+
+def find_units(grid):
+    units = []
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == GOBLIN:
+                units.append(Unit(row, column, False))
+            elif grid[row][column] == ELF:
+                units.append(Unit(row, column, True))
+    return units
 
 
 def split_grid(grid):
