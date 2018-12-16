4893fdf10ee459ad5f0d53f430693e457fa39225
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2018-12-16 09:41:50 +0100

    Add solution for task 1

--- 16/day_16.py
+++ 16/day_16.py
@@ -1,2 +1,82 @@
+ADDR = "addr"
+ADDI = "addi"
+MULR = "mulr"
+MULI = "muli"
+BANR = "banr"
+BANI = "bani"
+BORR = "borr"
+BORI = "bori"
+SETR = "setr"
+SETI = "seti"
+GTIR = "gtir"
+GTRI = "gtri"
+GTRR = "gtrr"
+EQRI = "eqri"
+EQIR = "eqir"
+EQRR = "eqrr"
+OPCODES = {ADDR, ADDI, MULR, MULI, BANR, BANI, BORR, BORI, SETR, SETI, GTIR, GTRR, EQRI, EQIR, EQRR}
+
+
+def op(register, opcode, input1, input2, output):
+    if opcode == ADDR:
+        register[output] = register[input1] + register[input2]
+    elif opcode == ADDI:
+        register[output] = register[input1] + input2
+    elif opcode == MULR:
+        register[output] = register[input1] * register[input2]
+    elif opcode == MULI:
+        register[output] = register[input1] * input2
+    elif opcode == BANR:
+        register[output] = register[input1] & register[input2]
+    elif opcode == BANI:
+        register[output] = register[input1] & input2
+    elif opcode == BORR:
+        register[output] = register[input1] | register[input2]
+    elif opcode == BORI:
+        register[output] = register[input1] | input2
+    elif opcode == SETR:
+        register[output] = register[input1]
+    elif opcode == SETI:
+        register[output] = input1
+    elif opcode == GTIR:
+        register[output] = 1 if input1 > register[input2] else 0
+    elif opcode == GTRI:
+        register[output] = 1 if register[input1] > input2 else 0
+    elif opcode == GTRR:
+        register[output] = 1 if register[input1] > register[input2] else 0
+    elif opcode == EQRI:
+        register[output] = 1 if input1 == register[input2] else 0
+    elif opcode == EQIR:
+        register[output] = 1 if register[input1] == input2 else 0
+    elif opcode == EQRR:
+        register[output] = 1 if register[input1] == register[input2] else 0
+
+
 def day_16_task_1(instructions):
-    return -1
+    instructions = parse_instructions(instructions)
+    three_or_more = 0
+
+    for instruction in instructions:
+        counter = 0
+        for opcode in OPCODES:
+            register = instruction[0].copy()
+            _, a, b, c = instruction[1]
+            op(register, opcode, a, b, c)
+            if register == instruction[2]:
+                counter += 1
+        if counter > 2:
+            three_or_more += 1
+
+    return three_or_more
+
+
+def parse_instructions(instructions):
+    chunks = [instructions[i:i + 3] for i in range(0, len(instructions), 4)]
+
+    result = []
+    for chunk in chunks:
+        before = list(map(int, chunk[0][chunk[0].index("[") + 1: chunk[0].index("]")].split(",")))
+        instruction = list(map(int, chunk[1].split()))
+        after = list(map(int, chunk[2][chunk[2].index("[") + 1: chunk[2].index("]")].split(",")))
+        result.append((before, instruction, after))
+    return result
--- 16/day_16_test.py
+++ 16/day_16_test.py
@@ -6,13 +6,13 @@ from .day_16 import day_16_task_1
 
 class TestDayXXTask1(unittest.TestCase):
 
-    @unittest.skip("speedy!")
     def test_day_16_task_1_simple(self):
         test_input = ["Before: [3, 2, 1, 1]",
                       "9 2 1 2",
-                      "After:  [3, 2, 2, 1]"]
+                      "After:  [3, 2, 2, 1]",
+                      ""]
         self.assertEqual(1, day_16_task_1(test_input))
 
     def test_day_16_task_1_input(self):
         test_input = read_file_lines(__file__, "input.txt")
-        self.assertEqual(0, day_16_task_1(test_input[:3128]))
+        self.assertEqual(590, day_16_task_1(test_input[:3128]))
